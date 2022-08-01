b2f7577c02c29806641a98fe14200f043a59fd06
uemun <88278175+uemun@users.noreply.github.com>
2022-08-01 12:58:10 +0000

    password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
  root 'home_page#home'
  get '/signup', to: 'users#new'
  get '/login', to: 'sessions#new'
@@ -6,4 +8,5 @@ Rails.application.routes.draw do
  delete '/logout',  to: 'sessions#destroy'
  resources :users
  resources :account_activations, only: [:edit]
+ resources :password_resets, only: [:new, :create, :edit, :update]
 end
